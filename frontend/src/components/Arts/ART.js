a296f50f6764c9c14c00c392ee2635906c85f67e
We Santos <wesantosdev@gmail.com>
2023-03-16 11:52:25 -0300

    BUG

--- frontend/src/components/Arts/ART.js
+++ frontend/src/components/Arts/ART.js
@@ -3,21 +3,33 @@ import styled from "styled-components";
 
 const ART = (props) => {
   return (
-    <ART_SQUARE>
-          <h1>{props.name}</h1>
-          <img src={props.imageURL} alt={props.name} key={props.index}  className="img"/>
-          <p>{props.description}</p>
+    <ART_SQUARE className="arts">
+      <div className="arts__square">
+          <div>
+            <h1>{props.name}</h1>
+            <img src={props.imageURL} alt={props.name} key={props.index}  className="img"/>
+            <p>{props.description}</p>
+          </div>
+      </div>
     </ART_SQUARE>
   )
 }
 
 const ART_SQUARE = styled.div`
-    width: 100%;
-    height: 100vh;
+    .arts {
+      width: 100%;
+      height: 100vh;
+    }
 
     .img {
       width: 300px;
     }
 
+    .arts__square {
+      > div {
+        display: flex;
+        flex-direction: column;
+      } 
+    }
 `
 export default ART
